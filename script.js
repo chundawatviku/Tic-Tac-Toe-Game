38f232213474fe2adbcb38cc4dde28aab7ec884a
chundawatviku <chundawatviku@gmail.com>
2026-06-29 13:40:56 +0530

    clean project structure

--- script.js
+++ script.js
@@ -1,137 +1,78 @@
-//Arithmetic Operators
-let a = 10;
-let b= 5;
+let box = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+let msgContainer = document.querySelector(".msg-container");
+let para = document.querySelector("p");
+let newGame = document.querySelector("#new");
 
-console.log ("a = ",a , "& b = ",b);
-console.log("a + b =",a + b);
-console.log("a - b =",a - b);
-console.log("a * b =",a * b);
-console.log("a / b =",a / b);
-console.log("a % b =",a % b); //Modulus
-console.log("a ** b =",a ** b); //Exponantiation
+let playerO = true;
 
-//Unary Operators
-let a = 10;
-let b= 5;
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
 
-console.log("a = ",a , "& b = ",b);
-//a++;
-console.log("a-- = ",a--);
-console.log("a = ",a);
-
-//Assignment Operators
-let a = 10;
-let b= 5;
-
-a **= 4;
-console.log("a = ",a);
-
-//Comparison Operators
-let a = 10;
-let b= 5;
-
-console.log("a >= b ",a >= b);
-
-//Logical Operators
-let a = 10;
-let b= 5;
-
-let cond1 = a < b;
-let cond2 = a!==10;
-console.log("cond1 || cond2 = ",cond1 || cond2);
-
-//Conditional Statements
-let age = 16;
-
-if (age >= 18) {
-    console.log("You Can Vote");
+const resetGame = () => {
+    playerO = true;
+    enabledboxes();
+    msgContainer.classList.add("hide");
 }
+box.forEach((box) => {
+    box.addEventListener("click",() => {
+       if(playerO) {
+        box.innerHTML = "O";
+        playerO = false;
+       }else {
+        box.innerHTML = "X";
+        playerO = true;
+       }
+       box.disabled = true;
 
-if (age < 18) {
-    console.log("You Cannot Vote");
-}
-
-let mode = "light";
-let color;
-
-if (mode === "dark") {
-    color="black";
-}
-
-if (mode === "light") {
-    color="white";
-}
-
-if (mode === "dark") {
-    color="black";
-} else {
-    color="white";
-}
-
-console.log(color);
-
-let no = 26;
-
-if (no%2 === 0) {
-    console.log(no,"is even");
-} else {
-    console.log(no,"is odd");
-}
-
-let mode = "dark";
-let color;
-
-if(mode === "dark") {
-    color = "black";
-} else if(mode === "light") {
-    color = "white";
-} else if(mode === "blue") {
-    color = "blue";
-} else {
-    color = "pink";
-}
-
-console.log(color);
-
-if(mode === "dark") console.log(mode);
-
-//Ternary Operators
-
-let age = 10;
-
-let result = age >= 18 ? "Adult" : "not Adult";
-console.log(result);
+       checkWinner();
+    })
+});
 
 
-let name = prompt("HELLO!");
-console.log(name);
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+         let pos1 = box[pattern[0]].innerHTML;
+         let pos2 = box[pattern[1]].innerHTML;
+         let pos3 = box[pattern[2]].innerHTML;
+
+        if(pos1 != "" && pos2 != "" && pos3 != ""){
+           if(pos1 === pos2  && pos2 === pos3 ){
+            console.log("Winner",pos1);
+            showWinner(pos1);
+
+          }
+        }
+    } 
+};
 
 
-//Que 1 
+const showWinner = (Winner) => {
+     para.innerText = `Congratulations, Winner is ${Winner}`;
+     msgContainer.classList.remove("hide");
+     disabledboxes();
+};
 
-let num = prompt("Enter a number:");
+const  disabledboxes = () => {
+    for(let val of box) {
+       val.disabled = true;
+    }
+};
 
-if(num % 3 === 0){
-    console.log(num,"is a multiple of 3");
-}else {
-    console.log(num,"is NOT a multiple of 3");
-}
+const  enabledboxes = () => {
+    for(let val of box) {
+       val.disabled = false;
+       val.innerText = "";
+    }
+};
 
-
-//Que 2
-
-let marks = 100;
-
-if(marks >= 80 && marks <= 100) {
-    grade = "A";
-} else if(marks >= 70) {
-    grade = "B";
-} else if(marks >= 60) {
-    grade = "C";
-} else if(marks >= 50) {
-    grade = "D";
-} else if (marks >= 0) {
-    grade = "E";
-}
-
-console.log("According to your marks,Your grade was :", grade);
+newGame.addEventListener("click",resetGame);
+reset.addEventListener("click",resetGame);
